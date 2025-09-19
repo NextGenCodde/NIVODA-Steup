72f1e1eb9270e09f65e231d0e8b33df63abab24b
Faizan <faizan@example.com>
2025-09-19 14:58:37 +0500

    save

--- server.js
+++ server.js
@@ -42,7 +42,9 @@ async function authenticate() {
   const json = await res.json();
   return json.data.authenticate.username_and_password.token;
 }
-
+app.get("health", (req, res) => {
+  res.send("OK");
+});
 // ---------------- SEARCH ROUTE ----------------
 app.post("/search", async (req, res) => {
   try {
